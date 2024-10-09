4d87d059e045ab6947d1df62202174606b65ae8a
bheang30 <solmorobhea20@gmail.com>
2024-10-09 13:15:16 +0800

    First Commit

---
+++ gradeAssignment.js
@@ -0,0 +1,16 @@
+File name (gradeAssignment.js)
+
+Code:
+
+function gradeCheck() {
+    let grade = prompt("Enter your score: ");
+    if (grade >=90) {
+        alert("Your grade is: Excellent.");
+    } else if (grade >=80) {
+        alert("Your grade is: Good.");
+    } else if (grade >=70) {
+        alert("Your grade is: Fair.");
+    } else {
+        alert("Your grade: Needs Improvement.")
+    }
+}
